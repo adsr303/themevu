4ca20a2e9d2b1251be79b073f745fd4df1c09b7c
Artur Rocha <adsr@poczta.onet.pl>
2024-10-28 19:46:01 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		line = strings.TrimPrefix(line, "#")
+		var r, g, b string
+		switch len(line) {
+		case 6:
+			r, g, b = line[0:2], line[2:4], line[4:6]
+		case 3:
+			r, g, b = line[0:1], line[1:2], line[2:3]
+		default:
+			log.Fatalf("invalid color code: %s", line)
+		}
+		err := printlnRGB(r, g, b)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	err := scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+const fullBlocks = "\u2588\u2588\u2588"
+
+func printlnRGB(r, g, b string) error {
+	rgb, err := hexToInt(r, g, b)
+	if err != nil {
+		return err
+	}
+	color.RGB(rgb[0], rgb[1], rgb[2]).Println(fullBlocks)
+	return nil
+}
+
+func hexToInt(values ...string) ([]int, error) {
+	result := make([]int, len(values))
+	for i, v := range values {
+		ii, err := parseHex(v)
+		if err != nil {
+			return nil, fmt.Errorf("parsing at position %d: %w", i, err)
+		}
+		result[i] = ii
+	}
+	return result, nil
+}
+
+func parseHex(value string) (int, error) {
+	ii, err := strconv.ParseInt(value, 16, 16)
+	if err != nil {
+		return 0, fmt.Errorf("parsing hex %s: %w", value, err)
+	}
+	if len(value) == 1 {
+		ii = (ii << 4) | ii
+	}
+	return int(ii), nil
+}

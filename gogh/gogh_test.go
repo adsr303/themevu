5905c9fbc690824e26f627e7bfeccddc2837c5f2
Artur Rocha <adsr@poczta.onet.pl>
2024-11-09 20:45:28 +0100

    The most basic test

---
+++ gogh/gogh_test.go
@@ -0,0 +1,21 @@
+package gogh_test
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/adsr303/hexcolors/gogh"
+)
+
+//go:embed Nanosecond.yml
+var nanosecond []byte
+
+func TestParseTheme(t *testing.T) {
+	g, err := gogh.ParseTheme(nanosecond)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	if g.Name != "Nanosecond" {
+		t.Errorf("expected name: Nanosecond, got: %s", g.Name)
+	}
+}
